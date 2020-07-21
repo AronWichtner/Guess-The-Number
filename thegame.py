326c74bd6655c26161525031e51a93aa63b0e1d4
AronWichtner <aronwichtner@gmail.com>
2020-07-21 20:48:22 +0200

    fatal changes

--- thegame.py
+++ thegame.py
@@ -1,65 +1,40 @@
-import random
+from userinputprocessing import *
+from setalevel import *
 
 
 def gamee():
     print(start())
-    rannum = generatenumber()
     while True:
-        i = intiger()
-        if type(i) == int:
-            a = within_the_limit(i)
-            if a == False:
-                continue
-            else:
-                solution = higher_lower_win(i, rannum)
-                if solution == False:
+        x = level()
+        if x == False:
+            continue
+        else:
+            rannum = generatenumber(x)
+        while True:
+            i = intiger()
+            if type(i) == int:
+                a = within_the_limit(i, x)
+                if a == False:
                     continue
                 else:
-                    print(solution)
-                    return anothergame()
-        else:
-            continue
-
-
-def generatenumber():
-    random_number = random.randint(0, 20)
-    return random_number
+                    solution = higher_lower_win(i, rannum)
+                    if solution == False:
+                        continue
+                    else:
+                        print(solution)
+                        return anothergame()
+            else:
+                continue
 
 
 def start():
-    return """\nIn this game you have to guess a random number between 0 and 20.\n"""
-
-
-def intiger():
-    try:
-        users_input = int(input("\nWhat is the number? \n"))
-    except ValueError:
-        print("You may only use integer!\n")
-    else:
-        return users_input
-
-
-def within_the_limit(users_input):
-    if users_input > 20 or users_input < 0:
-        print("\n{} is not within 0 to 20.\n".format(users_input))
-        users_input = False
-        return users_input
-    else:
-        users_input = True
-        return users_input
-
-
-def higher_lower_win(users_number, random_number):
-    if users_number < random_number:
-        users_number = False
-        print("\nguess higher!\n")
-        return users_number
-    elif users_number > random_number:
-        users_number = False
-        print("\nguess lower!\n")
-        return users_number
-    else:
-        return "\nYou win! The number equals to {}.\n".format(users_number)
+    return """\nIn this game you have to guess a random number between 0 and another number.
+There are five difficulty levels.
+->0-10 (level 1)
+->0-20 (level 2)
+->0-50 (level 3)
+->0-100 (level 4)
+->all levels at once (level 5)\n"""
 
 
 def anothergame():
@@ -77,5 +52,4 @@ def anothergame():
 print(gamee())
 
 
-# GUI einführen
-# Leben einführen
+
